023cfb7c892666b583f3a51bc9eb68b21f947d88
UdemyPracticasEmmanuel <emmanuel.lopez@neoris.com>
2024-06-18 12:27:36 -0700

    Se sube el repositorio de auth recien creado

---
+++ src/test/java/com/springdemo/TestingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
